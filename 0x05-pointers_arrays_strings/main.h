8c9312f312922b29aaea2fc3f8bfdfdc3eded495
Oduniyi Adebola <oduniyiadebola4@gmail.com>
2022-12-19 06:05:49 -0800

    reset

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,14 @@
+#ifndef main_h
+#define main_h
+extern void reset_to_98(int *n);
+extern void swap_int(int *a, int *b);
+extern int _strlen(char *s);
+extern void _puts(char *str);
+extern void print_rev(char *s);
+extern void rev_string(char *s);
+extern void puts2(char *str);
+extern void puts_half(char *str);
+extern void print_array(int *a, int n);
+extern char *_strcpy(char *dest, char *src);
+extern int _atoi(char *s);
+#endif
