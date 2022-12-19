ed53e11afd056065400accaf0922e5350aa50b1b
Oduniyi Adebola <oduniyiadebola4@gmail.com>
2022-12-19 08:27:49 -0800

    array

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
- *  * _strlen - Returns the length of a string.
- *   * @str: string.
- *    *
- *     * Return: Length.
- *      */
+ * _strlen - Returns the length of a string.
+ * @str: string.
+ *
+ * Return: Length.
+ */
 size_t _strlen(const char *str)
 {
-		size_t len = 0;
+	size_t len = 0;
 
-			while (*str++)
-					len++;
-				return (len);
+	while (*str++)
+	len++;
+	return (len);
 }
